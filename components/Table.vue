28c180b7d3d2c4732af1a80e14b491e5c0851e11
Priyanka-gade <priyanka.gadepg@gmail.com>
2022-08-01 14:33:28 +0530

    todo app

---
+++ components/Table.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <table class="m-10 justify-center border border-solid border-2 border-black">
+            <tr class="border border-solid border-black">
+                <th class="py-3 px-6">
+                    Name
+                </th>
+                <th class="py-3 px-6">
+                    Email
+                </th>
+                <th class="py-3 px-6">
+                    Mobile
+                </th>
+                <th class="py-3 px-6">
+                    Address
+                </th>
+                <th class="py-3 px-6">
+                    Actions
+                </th>
+            </tr>
+            <tr>
+                <td>Priyanka</td>
+                <td>Pg@gmail.com</td>
+                <td>9922498956</td>
+                <td>Pollard Farms Baner</td>
+                <td colspan="2"><button class="border rounded-lg p-1 bg-blue-600 text-white">Edit</button>&nbsp;<button class="border rounded-lg p-1 bg-red-500 text-white">Delete</button></td>
+            </tr>
+        </table>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Table Component',
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    
+    <h1>Hello</h1>
+    <Table></Table>
+</template>
+<script>
+
+import Table from '/components/Table.vue'
+
+export default {
+    name: "index component",
+    components: {Table }
+}
+</script>
