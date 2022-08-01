2ab339b5c1e950e505ee77a0906f1e017dd1c7bf
Priyanka-gade <priyanka.gadepg@gmail.com>
2022-08-01 17:28:53 +0530

    user data display

--- components/Form.vue
+++ components/Form.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="sm:grid justify-items-center">
-        <div>
-            <form class="grid justify-items-left bg-slate-200 p-7 w-64">
+        <div class="float-left">
+            <form @submit="addUserTodata" class="grid justify-items-left bg-slate-200 p-7 w-64">
                 <h1 class="text-center">Add New user </h1>
                 <div>
                     <input type="text" v-model="Name" placeholder="Name" required />
@@ -19,64 +19,75 @@
                 <button @click="adduser" class="border rounded-lg p-1 bg-blue-600 text-white">Add User</button>
             </form>
         </div>
-        <div>
+        <div class="float-right">
             <table class="sm:flex justify-center border-b m-2">
-            <tr class="sm:flex bg-slat)e-200">
-                <th class="py-3 px-6">
-                    Name
-                </th>
-                <th class="py-3 px-6">
-                    Email
-                </th>
-                <th class="py-3 px-6">
-                    Mobile
-                </th>
-                <th class="py-3 px-6">
-                    Address
-                </th>
-                <th class="py-3 px-6">
-                    Actions
-                </th>
-            </tr>
-            <tr>
-                <td>P</td>
-                <td>Pg@gmail.com</td>
-                <td>9922498956</td>
-                <td>Pollard Farms Baner</td>
-                <td colspan="2"><button class="border rounded-lg p-1 bg-blue-600 text-white">Edit</button>&nbsp;<button
-                        class="border rounded-lg p-1 bg-red-500 text-white">Delete</button></td>
-            </tr>
-             <tr>
-                <td>{{this.Name}}</td>
-                <td>{{this.Email}}</td>
-                <td>{{this.Mobile}}</td>
-                <td>{{this.Address}}</td>
-                <td colspan="2"><button class="border rounded-lg p-1 bg-blue-600 text-white">Edit</button>&nbsp;<button
-                        class="border rounded-lg p-1 bg-red-500 text-white">Delete</button></td>
-            </tr>
-        </table>
+                <tr class="sm:flex bg-slate-200">
+                    <th class="py-3 px-6">
+                        Name
+                    </th>
+                    <th class="py-3 px-6">
+                        Email
+                    </th>
+                    <th class="py-3 px-6">
+                        Mobile
+                    </th>
+                    <th class="py-3 px-6">
+                        Address
+                    </th>
+                    <th class="py-3 px-6">
+                        Actions
+                    </th>
+                </tr>
+                <tr class="sm:flex bg-slate-100">
+                    <td>P</td>
+                    <td>Pg@gmail.com</td>
+                    <td>9922498956</td>
+                    <td>Pollard Farms Baner</td>
+                    <td colspan="2"><button
+                            class="border rounded-lg p-1 bg-blue-600 text-white">Edit</button>&nbsp;<button
+                            class="border rounded-lg p-1 bg-red-500 text-white">Delete</button></td>
+                </tr>
+                <tr>
+                    <td>{{ this.Name }}</td>
+                    <td>{{ this.Email }}</td>
+                    <td>{{ this.Mobile }}</td>
+                    <td>{{ this.Address }}</td>
+                    <td colspan="2"><button
+                            class="border rounded-lg p-1 bg-blue-600 text-white">Edit</button>&nbsp;<button
+                            class="border rounded-lg p-1 bg-red-500 text-white">Delete</button></td>
+                </tr>
+            </table>
         </div>
     </div>
 </template>
 <script>
 
-import  UserDetails  from '/components/UserDetails.vue'
+import UserDetails from '/components/UserDetails.vue'
 export default {
     name: "User Form",
-    components:{UserDetails},
-    props:['dataarray','items'],
-// methods:{
-//     adduser(){
-//         this.dataarray.push(this.user)
-//     }
-// }
-data(){
-    return{
-        Name:'',
-        Email:'',
-        Mobile:'',
-        Address:''
+    components: { UserDetails },
+    props: ['dataarray', 'items'],
+    data() {
+        return {
+            Name: '',
+            Email: '',
+            Mobile: '',
+            Address: ''
+        }
+    },
+    methods: {
+        addUserTodata(e) {
+            e.preventDefault()
+            const newTodo = {
+                id: Date.now(),
+                Name: this.Name,
+                Email:this.Email,
+                Mobile:this.Mobile,
+                Address:this.Address
+            }
+            this.$emit("add-user", newUser)
+                this.Name = '';
+        }
     }
-}
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="sm:flex w-2/3 p-5 m-5">
-        <Form class="float-left"></Form>
+        <Form v-on:add-user="addUserTodata" class="float-left"></Form>
         <!-- <Table v-bind:dataarray="dataarray" class="float-right"></Table> -->
 
     </div>
@@ -33,18 +33,15 @@ export default {
                     Address: "Pollard Farms Baner2",
                     // completed: false
                 }
-            ],
-            user: {
-
-                // id: '2',
-                Name: ""
-                // Email: "ak@gmail.com",
-                // Mobile: "9765568956",
-                // Address: "Pollard Farms Baner2",
-                // // completed: false
+            ]
 
             }
+        },
+        methods:{
+            addUserTodata(newUser){
+                this.dataarray=[...this.dataarray,newUser]
+                console.log(this.addUserTodata);
+            }
         }
-    }
 }
 </script>
