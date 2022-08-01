ad2889311f1822c8880f9b0f43db2d541b9a79ee
Priyanka-gade <priyanka.gadepg@gmail.com>
2022-08-01 18:01:06 +0530

    user data display

--- components/Form.vue
+++ components/Form.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="sm:grid justify-items-center">
         <div class="float-left">
-            <form @submit="addUserTodata" class="grid justify-items-left bg-slate-200 p-7 w-64">
+            <form class="grid justify-items-left bg-slate-200 p-7 w-64">
                 <h1 class="text-center">Add New user </h1>
                 <div>
                     <input type="text" v-model="Name" placeholder="Name" required />
@@ -16,11 +16,12 @@
                     <input type="text" v-model="Address" placeholder="Address" required />
                 </div><br />
                 <br />
-                <button @click="adduser" class="border rounded-lg p-1 bg-blue-600 text-white">Add User</button>
+                <button @click="addUserTodata" type="button" class="border rounded-lg p-1 bg-blue-600 text-white">Add User</button>
+                <button type="reset">Reset</button>
             </form>
         </div>
         <div class="float-right">
-            <table class="sm:flex justify-center border-b m-2">
+            <table class="sm:flex border-b m-2">
                 <tr class="sm:flex bg-slate-200">
                     <th class="py-3 px-6">
                         Name
@@ -38,7 +39,7 @@
                         Actions
                     </th>
                 </tr>
-                <tr class="sm:flex bg-slate-100">
+                <!-- <tr class="sm:flex bg-slate-100 border-b border-2">
                     <td>P</td>
                     <td>Pg@gmail.com</td>
                     <td>9922498956</td>
@@ -46,12 +47,12 @@
                     <td colspan="2"><button
                             class="border rounded-lg p-1 bg-blue-600 text-white">Edit</button>&nbsp;<button
                             class="border rounded-lg p-1 bg-red-500 text-white">Delete</button></td>
-                </tr>
-                <tr>
-                    <td>{{ this.Name }}</td>
-                    <td>{{ this.Email }}</td>
-                    <td>{{ this.Mobile }}</td>
-                    <td>{{ this.Address }}</td>
+                </tr> -->
+                <tr v-for="row in dataarray" :key="row" class="sm:flex bg-slate-100 border-b">
+                    <td>{{ row.Name }}</td>
+                    <td>{{ row.Email }}</td>
+                    <td>{{ row.Mobile }}</td>
+                    <td>{{ row.Address }}</td>
                     <td colspan="2"><button
                             class="border rounded-lg p-1 bg-blue-600 text-white">Edit</button>&nbsp;<button
                             class="border rounded-lg p-1 bg-red-500 text-white">Delete</button></td>
@@ -72,21 +73,26 @@ export default {
             Name: '',
             Email: '',
             Mobile: '',
-            Address: ''
+            Address: '',
+
+            newdata: [],
         }
     },
     methods: {
         addUserTodata(e) {
             e.preventDefault()
-            const newTodo = {
+            const newUser = {
                 id: Date.now(),
                 Name: this.Name,
                 Email:this.Email,
                 Mobile:this.Mobile,
                 Address:this.Address
             }
-            this.$emit("add-user", newUser)
-                this.Name = '';
+            // this.$emit("add-user", newUser)
+                // this.Name = '',
+                // this.Email='',
+                // this.Mobile='',
+                // this.Address=''
         }
     }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="sm:flex w-2/3 p-5 m-5">
-        <Form v-on:add-user="addUserTodata" class="float-left"></Form>
+        <Form v-bind:dataarray="dataarray" v-on:add-user="addUserTodata" class="float-left"></Form>
         <!-- <Table v-bind:dataarray="dataarray" class="float-right"></Table> -->
 
     </div>
@@ -40,7 +40,7 @@ export default {
         methods:{
             addUserTodata(newUser){
                 this.dataarray=[...this.dataarray,newUser]
-                console.log(this.addUserTodata);
+                // console.log(this.addUserTodata);
             }
         }
 }
